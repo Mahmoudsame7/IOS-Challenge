b598e948c761b598acb4d9ade4472bbd8b7d7b3a
Mahmoudsame7 <49451485+Mahmoudsame7@users.noreply.github.com>
2023-03-30 17:02:55 +0200

    edit in urlsessionrequest
    adding completion handler in fetchphotos function header

--- Starter Project/ViewController.swift
+++ Starter Project/ViewController.swift
@@ -8,47 +8,66 @@
 import UIKit
 
 class ViewController: UIViewController,UICollectionViewDataSource,UICollectionViewDelegate,UICollectionViewDelegateFlowLayout{
-   
     
     
     
-
+    
+    
     @IBOutlet weak var CollectionView: UICollectionView!;
     
     var result:[String]=[];
     
+    
     let colorData=[UIColor.blue , UIColor.red , UIColor.green,UIColor.blue , UIColor.red , UIColor.green,UIColor.blue , UIColor.red , UIColor.green , UIColor.green ]
     
-   
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        
         // Do any additional setup after loading the view.
+        fetchphotos{[weak self] (images) in
+            self?.result=images
+            //print(images)
+        }
         
-        fetchphotos()
         CollectionView.dataSource=self
         CollectionView.delegate=self
-
+        
         DispatchQueue.main.async {
             self.CollectionView.reloadData()
-        }
-       
+         }
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let CV_width=CollectionView.bounds.width;
+        
+        let CV_flowlayout=collectionViewLayout as! UICollectionViewFlowLayout;
+        let spacebetweencells=CV_flowlayout.minimumInteritemSpacing;
+        
+        let adjusted_width=CV_width-spacebetweencells;
+        
+        let width:CGFloat = adjusted_width/2 - 20;
+        let height:CGFloat = 140;
+        
+        return CGSize(width: width, height: height);
+        
     }
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        print(result.count)
-        return result.count
+        
+        print(self.result.count)
+        return self.result.count
     }
-
+    
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = CollectionView.dequeueReusableCell(withReuseIdentifier: "CollectionViewCell", for: indexPath) as! CustomCollectionViewCell
-       
+        
         //cell.backgroundColor=colorData[indexPath.item]
-        let url = URL(string: result[indexPath.item])!
-
-
+        let url = URL(string: self.result[indexPath.item])!
+        
+        
         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-
+            
             if let error = error {
                 print(error)
                 return
@@ -65,38 +84,39 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
                     print(error)
                 }
             }
-
-
+            
+            
         }
         task.resume()
-    
+        
         
         
         return cell
     }
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
-        print(result[indexPath.item])
         collectionView.deselectItem(at: indexPath, animated: true)
     }
     
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-
+        
         if let id = segue.identifier{
             if(id == "ShowImageViewController"){
                 let newVC=segue.destination as! ImageViewController
-
+                
                 //get the index path for the current clicked photo
                 var indexPath=self.CollectionView.indexPath(for: sender as! CustomCollectionViewCell)
-
+                
                 let url=self.result[(indexPath!.item)];
                 newVC.url=url;
-
+                
             }
         }
-
-
+        
+        
     }
-    func fetchphotos(){
+    func fetchphotos(completionHandler: @escaping ([String]) -> Void){
+        
+        var res:[String]=[]
         let url = URL(string: "https://api.unsplash.com/photos/?client_id=Ahj-66mbyiRNL-GhTltHoIgGfkznNgv7SALhCOTLMaM")!
         
         
@@ -117,9 +137,11 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
                 do{
                     let tasks=try decoder.decode([Image].self ,from: data)
                     tasks.forEach{ i in
-                        self.result.append(i.urls["small"]!)
+                        res.append(i.urls["small"]!)
                     }
-                    print(self.result)
+                    //print(res)
+                    completionHandler(res)
+                    //print(self.result)
                 }catch{
                     print(error)
                 }
@@ -128,6 +150,7 @@ class ViewController: UIViewController,UICollectionViewDataSource,UICollectionVi
             
         }
         task.resume()
+        
     }
     
 }
